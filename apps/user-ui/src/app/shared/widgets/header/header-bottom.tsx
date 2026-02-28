cbd7dc79f941538383abc8061bb91768e80807e6
Aamosh Thapa <aamoshthapa06@gmail.com>
2026-02-28 15:11:52 -0600

    Update user UI pages and header components

--- apps/user-ui/src/app/shared/widgets/header/header-bottom.tsx
+++ apps/user-ui/src/app/shared/widgets/header/header-bottom.tsx
@@ -1,8 +1,9 @@
 'use client'
 import { navItems } from '@/configs/constant';
-import { AlignLeft, ChevronDown} from 'lucide-react';
+import { AlignLeft, ChevronDown, HeartIcon, ShoppingCart} from 'lucide-react';
 import React, {useEffect, useState} from 'react'
 import Link from "next/link";
+import ProfileIcon from '@/assets/svgs/profile-icon';
 
 const HeaderBottom = () => {
     const [show, setShow] = useState(false);
@@ -11,7 +12,7 @@ const HeaderBottom = () => {
     //Track scroll position
     useEffect(() => {
         const handleScroll = () => {
-           if(window.scrollY > 190){
+           if(window.scrollY > 100){
             setIsSticky(true);
            }else{ 
             setIsSticky(false);
@@ -48,7 +49,36 @@ const HeaderBottom = () => {
             ))}
         </div>
         <div>
-          
+          {isSticky &&(
+            <div className= "flex items-center gap-8">
+                    <div className = 'flex items-center gap-2'>
+                      <Link href={"/login"}
+                      className = "border-2 w-[50px] h-[50px] flex items-center justify-center rounded-full border-[#010f1c1a] ">
+                        <ProfileIcon />
+                      </Link>
+                    <Link href = {"/login"}>
+                    <span className = "block font-medium">Hello,</span>
+                    <span className = "font-semibold">Sign In</span>
+                    </Link>
+                    </div>
+                    <div className = "flex items-center gap-5">
+                      <Link href ={"/wishlist"} className ="relative">
+                      <HeartIcon/>
+                      <div className ="w-6 h-6 border-2 border-white bg-red-500 rounded-full flex items-center justify-center absolute top-[-10] right-[-10]">
+                        <span className ="text-white font-medium text-sm">0</span>
+                      </div>
+                      </Link>
+                    </div>
+                    <div className = "flex items-center gap-5">
+                      <Link href ={"/cart"} className ="relative">
+                      <ShoppingCart/>
+                      <div className ="w-6 h-6 border-2 border-white bg-red-500 rounded-full flex items-center justify-center absolute top-[-10] right-[-10]">
+                        <span className ="text-white font-medium text-sm">0</span>
+                      </div>
+                      </Link>
+                    </div>
+            </div>
+          )}
         </div>
        
 
--- apps/user-ui/src/app/shared/widgets/header/header.tsx
+++ apps/user-ui/src/app/shared/widgets/header/header.tsx
@@ -4,7 +4,6 @@ import { Search, HeartIcon, ShoppingCart } from 'lucide-react';
 import ProfileIcon from '@/assets/svgs/profile-icon';
 import HeaderBottom from './header-bottom';
 
-
 const Header = () => {
   return (
     <div className ='w-full bg-white'>
@@ -23,7 +22,7 @@ const Header = () => {
             </div>
             <div className= "flex items-center gap-8">
                     <div className = 'flex items-center gap-2'>
-                      <Link href={"/login"}
+                      <Link href={"./login"}
                       className = "border-2 w-[50px] h-[50px] flex items-center justify-center rounded-full border-[#010f1c1a] ">
                         <ProfileIcon />
                       </Link>
